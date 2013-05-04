d9e0bead4bf67e103a33adb3948b8191d8aaa7ea
jkunlin <jkunlin@gmail.com>
2013-05-04 14:53:22 +0800

    meger mcqd and mcs success

--- super.h
+++ super.h
@@ -43,6 +43,7 @@ class Maxclique {
 		void push(const int ii) { v[sz++].set_i(ii); };
 		void pop() { sz--; };
 		void pop(const int ii);
+		void pop_index(const int index);
 		Vertex& at(const int ii) const { return v[ii]; };
 		Vertex& end() const { return v[sz - 1]; };
 	};
@@ -92,14 +93,19 @@ class Maxclique {
 	bool connection(const int i, const int j) const { return e[i][j]; }
 	bool conflict(const int, const ColorClass&);
 	void cut(const Vertices&, Vertices&);
+	void cut_mcs(const Vertices&, Vertices&, const int pi);
 	void color_sort(Vertices&);
 	void re_color_sort(Vertices&);
+	void re_color_sort_mcs(Vertices&, Vertices&);
 	int re_color(const int);
 	int count_conflict(int, const ColorClass&, int&);
 	bool detect_clique(Vertices&);
 	void detect_clique_vertices(Vertices&);
+	void detect_clique_vertices(Vertices&, Vertices&);
 	void expand(Vertices);
 	void expand_dyn(Vertices);
+	void expand_mcs(Vertices, Vertices);
+	void expand_dyn_mcs(Vertices, Vertices);
 	void _mcq(int*&, int&, bool);
 	void degree_sort(Vertices &R) { R.set_degrees(*this); R.sort(); }
 public:
@@ -170,15 +176,22 @@ void Maxclique::_mcq(int* &maxclique, int &sz, bool dyn) {
 	V.sort();
 	permute_vertices();
 	V.init_colors();
+	Vertices Va(V.size());
+	for (int i = 0; i < V.size(); ++i) {
+		Va.push(V.at(i).get_i());
+		Va.at(i).set_degree(V.at(i).get_degree());
+	}
+	
 	if (dyn) {
 		for (int i=0; i < V.size() + 1; i++) {
 			S[i].set_i1(0);
 			S[i].set_i2(0);
 		}
-		expand_dyn(V);
+		expand_dyn_mcs(V, Va);
 	}
 	else
 		expand(V);
+		
 	maxclique = new int[QMAX.size()]; 
 	for (int i=0; i<QMAX.size(); i++) { 
 		maxclique[i] = QMAX.at(i);
@@ -212,6 +225,14 @@ void Maxclique::Vertices::pop(const int ii) {
 	pop();
 }
 
+void Maxclique::Vertices::pop_index(const int index) {
+	for (int iter = index + 1; iter < sz; ++iter) {
+		v[iter - 1] = v[iter];
+		v[iter - 1].set_degree(v[iter].get_degree() - 1);
+	}
+	pop();
+}
+
 void Maxclique::ColorClass::pop(const int index){
 	for (int iter = index + 1; iter < sz; ++iter) {
 		i[iter - 1] = i[iter];
@@ -225,6 +246,14 @@ bool Maxclique::conflict(const int pi, const ColorClass &A) {
 			return true;
 	return false;
 }
+
+void Maxclique::cut_mcs(const Vertices &A, Vertices &B, const int pi) {
+	for (int i = 0; i < A.size() - 1; ++i) {
+		if (connection(pi, A.at(i).get_i()))
+			B.push(A.at(i).get_i());
+	}
+}
+
 
 void Maxclique::cut(const Vertices &A, Vertices &B) {
 	for (int i = 0; i < A.size() - 1; i++) {
@@ -287,6 +316,43 @@ void Maxclique::color_sort(Vertices &R) {
 			R.at(j).set_i(C[k].at(i));
 			R.at(j++).set_degree(k);
 		}
+}
+
+void Maxclique::re_color_sort_mcs(Vertices &R, Vertices &Ra) {
+	int j = 0;
+	int maxno = 1;
+	int min_k = QMAX.size() - Q.size() + 1;
+	C[1].rewind();
+	C[2].rewind();
+	int k = 1;
+	for (int i=0; i < Ra.size(); i++) {
+		int pi = Ra.at(i).get_i();
+		k = 1;
+		while (conflict(pi, C[k]))
+			k++;
+		C[k].push(pi);
+		if (k > maxno) {
+			maxno = k;
+			C[maxno + 1].rewind();
+		}
+		if (k == maxno && k >= min_k) {
+			k = re_color(k);
+			if(C[maxno].size() == 0)
+				maxno -= 1;
+		}
+		if (k < min_k) {
+			R.push(pi);
+			++j;
+		}
+	}
+	if (j > 0) R.at(j-1).set_degree(0);
+	if (min_k <= 0) min_k = 1;
+	for (k = min_k; k <= maxno; k++)
+		for (int i = 0; i < C[k].size(); i++) {
+			R.push(C[k].at(i));
+			R.at(j++).set_degree(k);
+		}
+
 }
 
 void Maxclique::re_color_sort(Vertices &R) {
@@ -433,6 +499,128 @@ next_color:
 		QMAX = Q;
 }
 
+void Maxclique::detect_clique_vertices(Vertices &R, Vertices &Ra) {
+	int max_no = R.end().get_degree();
+	int clq_v = -1;
+	for (int k = 1; k <= max_no; ++k) {
+		if (C[k].size() > 1)
+			continue;
+		clq_v = C[k].at(0);
+		for (int l = 1; l < k; ++l) {
+			for (int i = 0; i < C[l].size(); ++i) {
+				if (!connection(clq_v, C[l].at(i)))
+					goto next_color;
+			}
+		}
+		Q.push(clq_v);
+		CLIQUE_VERTEX.push(clq_v);
+		R.pop(clq_v); //how to do ?
+		Ra.pop(clq_v);
+		if(R.size()) {
+			++pk;
+			S[level].inc_i1();
+			++level;
+			S[level].set_i1(S[level].get_i1() + S[level - 1].get_i1() - S[level].get_i2());
+			S[level].set_i2(S[level - 1].get_i1());
+		}
+next_color:
+		;
+	}
+	if (Q.size() > QMAX.size())
+		QMAX = Q;
+}
+
+void Maxclique::expand_mcs(Vertices R, Vertices Ra) {
+	while (R.size()) {
+		if (Q.size() + R.end().get_degree() > QMAX.size()) {
+			Q.push(R.end().get_i());
+			Vertices Rap(R.size());
+			cut_mcs(Ra, Rap, R.end().get_i());
+			if (Rap.size()) {
+				Vertices Rp(R.size());
+				re_color_sort_mcs(Rp, Rap);
+				if (detect_clique(Rp)) {
+					Rp.dispose();
+					goto next_vertex;
+				}
+				else
+					detect_clique_vertices(Rp, Rap);
+				++pk;
+				expand_mcs(Rp, Rap);
+				Rp.dispose();
+			}
+			else if (Q.size() > QMAX.size()) { 
+				std::cout << "step = " << pk << " current max. clique size = " << Q.size() << std::endl; 
+				QMAX = Q;
+			}    
+next_vertex:
+			Rap.dispose();
+			while(CLIQUE_VERTEX.size() != 0 && Q.end() == CLIQUE_VERTEX.end()) {
+				Q.pop();
+				CLIQUE_VERTEX.pop();
+			}
+			Q.pop();
+		}
+		else {
+			return;
+		}
+		Ra.pop(R.end().get_i());
+		R.pop();
+	}
+
+}
+
+void Maxclique::expand_dyn_mcs(Vertices R, Vertices Ra) {
+	S[level].set_i1(S[level].get_i1() + S[level - 1].get_i1() - S[level].get_i2());
+	S[level].set_i2(S[level - 1].get_i1());
+	while (R.size()) {
+		if (Q.size() + R.end().get_degree() > QMAX.size()) {
+			Q.push(R.end().get_i());
+			Vertices Rap(R.size());
+			cut_mcs(Ra, Rap, R.end().get_i());
+			if (Rap.size()) {
+				Vertices Rp(R.size());
+				if ((float)S[level].get_i1()/++pk < Tlimit) {
+					degree_sort(Rap);
+				}
+				re_color_sort_mcs(Rp, Rap);
+				
+				if (detect_clique(Rp)) {
+					Rp.dispose();
+					goto next_vertex;
+				}
+				
+				else
+					detect_clique_vertices(Rp, Rap);
+					
+				S[level].inc_i1();
+				level++;
+				expand_dyn_mcs(Rp, Rap);
+				level--;
+				Rp.dispose();
+			}
+			else if (Q.size() > QMAX.size()) { 
+				std::cout << "step = " << pk << " current max. clique size = " << Q.size() << std::endl; 
+				QMAX = Q;
+			}    
+next_vertex:
+			Rap.dispose();
+			while(CLIQUE_VERTEX.size() != 0 && Q.end() == CLIQUE_VERTEX.end()) {
+				Q.pop();
+				CLIQUE_VERTEX.pop();
+				--level;
+			}
+			Q.pop();
+		}
+		else {
+			return;
+		}
+		Ra.pop(R.end().get_i());
+		R.pop();
+	}
+}
+
+
 
 
 void Maxclique::expand(Vertices R) {
@@ -505,5 +693,6 @@ next_vertex:
 		R.pop();
 	}
 }
+
 
 #endif
